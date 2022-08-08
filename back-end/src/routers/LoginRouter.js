f8d878fc15bd54e2e5f5d75a9746fac04f28b460
raffael marinho <silvamarinho10@gmail.com>
2022-08-08 19:55:31 -0300

    feat: add validation password

--- back-end/src/routers/LoginRouter.js
+++ back-end/src/routers/LoginRouter.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const { FindUser } = require('../controller/UserController');
 
+const { dataValidate } = require('../middwares/validateUser');
+
 const loginRouter = express.Router();
 
-loginRouter.post('/', FindUser);
+loginRouter.post('/', dataValidate, FindUser);
 
 module.exports = loginRouter;
