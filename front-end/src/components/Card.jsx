7ca8943346a0b6ad5df8f1ef28ca909a44baf5fb
Victor Martins <victor.martins.duarte@gmail.com>
2022-08-11 17:06:13 -0300

    ongoing: customer products page

--- front-end/src/components/Card.jsx
+++ front-end/src/components/Card.jsx
@@ -1,21 +1,23 @@
 import React, { useState } from 'react';
 
-function Card() {
+function Card(products) {
   const [counter, setCounter] = useState(0);
+  const { id, name, price, img } = products;
+  console.log(img);
 
   return (
     <div>
-      <h3 data-testid="customer_products__element-card-price-<id>">price</h3>
+      <h3 data-testid={ `customer_products__element-card-price-${id}` }>{ price }</h3>
       <img
-        data-testid="customer_products__img-card-bg-image-<id>"
-        src=""
-        alt=""
+        data-testid={ `customer_products__img-card-bg-image-${id}` }
+        src={ img }
+        alt={ name }
       />
       <div>
-        <h3 data-testid="customer_products__element-card-title-<id>">name</h3>
+        <h3 data-testid={ `customer_products__element-card-title-${id}` }>{ name }</h3>
         <div>
           <button
-            data-testid="customer_products__button-card-rm-item-<id>"
+            data-testid={ `customer_products__button-card-rm-item-${id}` }
             type="button"
             disabled={ counter < 1 }
             onClick={ () => setCounter(counter - 1) }
@@ -24,16 +26,15 @@ function Card() {
           </button>
           <input
             value={ counter }
-            data-testid="customer_products__input-card-quantity-<id>"
+            data-testid={ `customer_products__input-card-quantity-${id}` }
             type="text"
           />
           <button
-            data-testid="customer_products__button-card-add-item-<id>"
+            data-testid={ `customer_products__button-card-add-item-${id}` }
             type="button"
             onClick={ () => setCounter(counter + 1) }
           >
             +
-
           </button>
         </div>
       </div>
--- front-end/src/pages/CustomerProducts.jsx
+++ front-end/src/pages/CustomerProducts.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar';
 import Card from '../components/Card';
+import { apiProducts } from '../services/api';
 
 function CustomerProducts() {
+  const [products, setProducts] = useState([]);
+
+  async function customerProducts() {
+    const customProducts = await apiProducts();
+    setProducts(customProducts);
+  }
+
+  useEffect(() => {
+    customerProducts();
+  }, []);
+
+  console.log(products);
   return (
     <div>
       <Navbar />
-      <Card />
+      {products.map(({ id, name, price, urlImage }) => (
+        <Card
+          key={ id }
+          id={ id }
+          name={ name }
+          img={ urlImage }
+          price={ price }
+        />
+      ))}
     </div>
   );
 }
